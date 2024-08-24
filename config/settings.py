120f7caea2378fd4cc89f2b3badad0c8f9435aa1
= <sanare1382@gmail.com>
2024-08-24 12:25:14 +0330

    create product app model

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from environs import Env
+import os
 
 
 
@@ -162,6 +163,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ] 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Product
 
-# Create your views here.
+
+class ProductListView(generic.ListView):
+    queryset = Product.objects.filter(active=True)
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
+    
+class ProductDetailView(generic.DeleteView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
+    
+    
